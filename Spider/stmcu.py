d63db93e9df791ecae67f05a46861339c24d6b15
zhangyinxi <59528351+zhangyinxi@users.noreply.github.com>
2023-01-31 15:57:43 +0800

    Update stmcu.py

--- Spider/stmcu.py
+++ Spider/stmcu.py
@@ -1,11 +1,11 @@
-  1 
-  2 class Stmcu:
-  3     def saveparam(self,arg):
-  4         f       = open("stmcu.txt",'w')
-  5         f.write(arg)
-  6         f.close()
-  7     def getparam(self):
-  8         f2      = open("stmcu.txt",'r')
-  9         content = f2.read()
- 10         f2.close()
- 11         return content
+ 
+class Stmcu:
+    def saveparam(self,arg):
+        f       = open("stmcu.txt",'w')
+        f.write(arg)
+        f.close()
+    def getparam(self):
+        f2      = open("stmcu.txt",'r')
+        content = f2.read()
+        f2.close()
+        return content
