b426dcd7db5b4adb7d1a995dcbafd5be9c3916df
zhangyinxi <59528351+zhangyinxi@users.noreply.github.com>
2023-02-26 15:34:55 +0800

    Update update-repo.sh

--- update-repo.sh
+++ update-repo.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 status_log=$(git status -sb)
 # 这里使用的是 master 分支，根据需求自行修改
-if [ "$status_log" == "## main...creat/main" ];then
+if [ "$status_log" == "## main...main" ];then
   echo "nothing to commit, working tree clean"
 else
   git add .&&git commit -m "update by github actions"&&git push origin main
