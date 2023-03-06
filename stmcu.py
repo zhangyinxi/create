a44c602f74ecfa483b265b46ddf195a5e3d947e8
zhangyinxi <59528351+zhangyinxi@users.noreply.github.com>
2023-03-06 18:04:45 +0800

    Update stmcu.py

--- stmcu.py
+++ stmcu.py
@@ -1,11 +1,10 @@
 class Stmcu:
     def saveparam(self,arg):
-        f       = open("/usr/bin/stmcu.txt",'w')
+        f       = open("stmcu.txt",'w')
         f.write(arg)
         f.close()
-
     def getparam(self):
-        f2      = open("/usr/bin/stmcu.txt",'r')
+        f2      = open("stmcu.txt",'r')
         content = f2.read()
         f2.close()
         return content
